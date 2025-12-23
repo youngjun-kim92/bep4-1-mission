7ad9e67c3956087fc9a9ef81420fbfc994379cfe
youngjun\youngjun <whiteblood92@naver.com>
2025-12-23 11:32:00 +0900

    0001

---
+++ src/main/java/com/back/jpa/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.back.jpa.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+// 모든 엔티티들의 조상
+public class BaseEntity {
+    public String getModelTypeCode() {
+        return this.getClass().getSimpleName();
+    }
+}
