7a0eddfd6a52023277f4f36d57e762e6f059fd94
youngjun\youngjun <whiteblood92@naver.com>
2025-12-23 14:44:48 +0900

    0005

--- src/main/java/com/back/boundedContext/post/service/PostService.java
+++ src/main/java/com/back/boundedContext/post/service/PostService.java
@@ -1,8 +1,8 @@
-package com.back.service;
+package com.back.boundedContext.post.service;
 
-import com.back.entity.Member;
-import com.back.entity.Post;
-import com.back.repository.PostRepository;
+import com.back.boundedContext.member.entity.Member;
+import com.back.boundedContext.post.entity.Post;
+import com.back.boundedContext.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
