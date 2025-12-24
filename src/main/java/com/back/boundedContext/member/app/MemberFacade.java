fc587f2da121b6b79af45e02077a932a29eb4dd9
youngjun\youngjun <whiteblood92@naver.com>
2025-12-24 10:24:08 +0900

    0012

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -1,8 +1,9 @@
 package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
-import com.back.global.exception.DomainException;
 import com.back.boundedContext.member.out.MemberRepository;
+import com.back.global.exception.DomainException;
+import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,12 +22,12 @@ public class MemberFacade {
     }
 
     @Transactional
-    public Member join(String username, String password, String nickname) {
+    public RsData<Member> join(String username, String password, String nickname) {
         findByUsername(username).ifPresent(m -> {
             throw new DomainException("409-1", "이미 존재하는 username 입니다.");
         });
 
-        return memberRepository.save(new Member(username, password, nickname));
+        return memberJoinUserCase.join(username, password, nickname);
     }
 
     @Transactional(readOnly = true)
