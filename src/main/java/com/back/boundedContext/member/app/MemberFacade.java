cb2bdb0c01cb39a69872c7de2750f58aacc976ee
youngjun\youngjun <whiteblood92@naver.com>
2025-12-26 15:16:47 +0900

    0029

--- src/main/java/com/back/boundedContext/member/app/MemberFacade.java
+++ src/main/java/com/back/boundedContext/member/app/MemberFacade.java
@@ -1,8 +1,6 @@
 package com.back.boundedContext.member.app;
 
 import com.back.boundedContext.member.domain.Member;
-import com.back.boundedContext.member.domain.MemberPolicy;
-import com.back.boundedContext.member.out.MemberRepository;
 import com.back.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,32 +11,31 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class MemberFacade {
-    private final MemberRepository memberRepository;
-    private final MemberJoinUseCase memberJoinUserCase;
-    private final MemberPolicy memberPolicy;
-
-    @Transactional(readOnly = true)
-    public long count() {
-        return memberRepository.count();
-    }
+    private final MemberSupport memberSupport;
+    private final MemberJoinUseCase memberJoinUseCase;
+    private final MemberGetRandomSecureTipUseCase memberGetRandomSecureTipUseCase;
 
     @Transactional
     public RsData<Member> join(String username, String password, String nickname) {
-        return memberJoinUserCase.join(username, password, nickname);
+        return memberJoinUseCase.join(username, password, nickname);
+    }
+
+    public String getRandomSecureTip() {
+        return memberGetRandomSecureTipUseCase.getRandomSecureTip();
+    }
+
+    @Transactional(readOnly = true)
+    public long count() {
+        return memberSupport.count();
     }
 
     @Transactional(readOnly = true)
     public Optional<Member> findByUsername(String username) {
-        return memberRepository.findByUsername(username);
+        return memberSupport.findByUsername(username);
     }
 
     @Transactional(readOnly = true)
     public Optional<Member> findById(int id) {
-        return memberRepository.findById(id);
-    }
-
-    public String getRandomSecureTip() {
-        return "비밀번호의 유효기간은 %d일 입니다."
-                .formatted(memberPolicy.getNeedToChangePasswordDays());
+        return memberSupport.findById(id);
     }
 }
