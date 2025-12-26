a66de500cca51f1e5dcb92fca2300089c30e26e9
youngjun\youngjun <whiteblood92@naver.com>
2025-12-26 11:39:32 +0900

    0025

--- src/main/java/com/back/boundedContext/cash/app/CashFacade.java
+++ src/main/java/com/back/boundedContext/cash/app/CashFacade.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CashFacade {
@@ -35,5 +37,15 @@ public class CashFacade {
         Wallet wallet = new Wallet(holder);
 
         return walletRepository.save(wallet);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<CashMember> findMemberByUsername(String username) {
+        return cashMemberRepository.findByUsername(username);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Wallet> findWalletByHolder(CashMember holder) {
+        return walletRepository.findByHolder(holder);
     }
 }
--- src/main/java/com/back/boundedContext/cash/out/WalletRepository.java
+++ src/main/java/com/back/boundedContext/cash/out/WalletRepository.java
@@ -1,7 +1,11 @@
 package com.back.boundedContext.cash.out;
 
+import com.back.boundedContext.cash.domain.CashMember;
 import com.back.boundedContext.cash.domain.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WalletRepository extends JpaRepository<Wallet, Integer> {
+    Optional<Wallet> findByHolder(CashMember holder);
 }
