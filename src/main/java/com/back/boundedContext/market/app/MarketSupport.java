03ab7a5489884cb175ecf63418fb6b21580ee287
youngjun\youngjun <whiteblood92@naver.com>
2025-12-29 10:39:07 +0900

    0033

--- src/main/java/com/back/boundedContext/market/app/MarketSupport.java
+++ src/main/java/com/back/boundedContext/market/app/MarketSupport.java
@@ -1,6 +1,9 @@
 package com.back.boundedContext.market.app;
 
+import com.back.boundedContext.market.domain.Cart;
 import com.back.boundedContext.market.domain.MarketMember;
+import com.back.boundedContext.market.domain.Product;
+import com.back.boundedContext.market.out.CartRepository;
 import com.back.boundedContext.market.out.MarketMemberRepository;
 import com.back.boundedContext.market.out.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,7 @@ import java.util.Optional;
 public class MarketSupport {
     private final ProductRepository productRepository;
     private final MarketMemberRepository marketMemberRepository;
+    private final CartRepository cartRepository;
 
     public long countProducts() {
         return productRepository.count();
@@ -20,5 +24,13 @@ public class MarketSupport {
 
     public Optional<MarketMember> findMemberByUsername(String username) {
         return marketMemberRepository.findByUsername(username);
+    }
+
+    public Optional<Cart> findCartByBuyer(MarketMember buyer) {
+        return cartRepository.findByBuyer(buyer);
+    }
+
+    public Optional<Product> findProductById(int id) {
+        return productRepository.findById(id);
     }
 }
